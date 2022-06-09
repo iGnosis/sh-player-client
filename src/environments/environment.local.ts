21e2915a93af63abdce4440b4348822042f78959
Aman Gautam <aman@pointmotioncontrol.com>
2022-06-09 12:10:10 +0530

    ci: 🎡 adding local environment

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    name: 'local',
+    gqlEndpoint: 'https://api.dev.pointmotioncontrol.com/v1/graphql',
+    servicesEndpoint: 'https://services.dev.pointmotioncontrol.com',
+    activityEndpoint: 'http://localhost:4201/'
+  };
+  
