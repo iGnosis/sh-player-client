f4816c2d2a10320f5de3ffc40a46dcfc1e1a201f
Mohan Vaddi <myselfgeek01@gmail.com>
2022-07-07 11:30:11 +0530

    implemented new homepage

--- src/app/pages/app/home/home.component.ts
+++ src/app/pages/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { session } from "src/app/store/reducers/home.reducer";
 import { trigger, transition, animate, style } from "@angular/animations";
 import { GoalsService } from "src/app/services/goals/goals.service";
 import { map } from "rxjs";
+import * as d3 from "d3";
 import { JwtService } from "src/app/services/jwt.service";
 @Component({
   selector: "app-home",
@@ -37,12 +38,7 @@ import { JwtService } from "src/app/services/jwt.service";
       ]),
     ]),
     trigger("fadeOut", [
-      transition(":leave", [
-        animate(
-          "200ms ease-in",
-          style({ opacity: "0" })
-        ),
-      ]),
+      transition(":leave", [animate("200ms ease-in", style({ opacity: "0" }))]),
     ]),
   ],
 })
@@ -53,18 +49,34 @@ export class HomeComponent implements OnInit, AfterViewInit {
   sessionId!: string;
   session = session.Start;
   currentSession = 0;
+
+  monthlyCompletionPercent: number = 70;
+  monthlyGoalPercent: number = 100;
+  monthRange = d3.range(0, 30.5, 5);
+
+  // TODO: has to be changed based on monthly goals API
+  rewardsRange = [5, 10, 15];
+  daysCompletedThisMonth = 22;
+
+  // TODO: has to be changed based on daily goals API
+  minutesCompletedToday = 15;
+
+  currentDate = {
+    day: `${new Date().getDate()}${this.nth(new Date().getDate())}`,
+    month: new Date().toLocaleDateString("default", { month: "long" }),
+    year: new Date().getFullYear(),
+  };
+
+  dailyCompletitionPercent!: number;
+
   dummySessions = [
     {
       title: "Sit. Stand. Achieve",
       bg: "/assets/images/start-session-bg.jpg",
       status: session.Start,
     },
-    {
-      title: "Mind Body Connection",
-      bg: "/assets/images/mind_body_connection.jpg",
-      status: session.Locked,
-    },
   ];
+
   constructor(
     private careplanService: CareplanService,
     private sessionService: SessionService,
@@ -74,12 +86,13 @@ export class HomeComponent implements OnInit, AfterViewInit {
     private jwtService: JwtService
   ) {
     this.user = JSON.parse(localStorage.getItem("user") || "{}");
+    console.log(this.monthRange);
   }
 
   async ngOnInit(): Promise<void> {
     this.activatedRoute.paramMap
       .pipe(map(() => window.history.state))
-      .subscribe(state => {
+      .subscribe((state) => {
         this.shScreen = !!state.loggedIn;
       });
 
@@ -88,6 +101,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
+    this.initMonthlyBar();
     setTimeout(() => {
       this.shScreen = false;
     }, 4000);
@@ -107,15 +121,15 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   async startNewSession() {
     if (this.jwtService.checkCareplanAndProviderInJWT()) {
-      console.log('startNewSession:JWT has careplan and provider set');
+      console.log("startNewSession:JWT has careplan and provider set");
       const activeCareplans = await this.careplanService.getActiveCareplans();
       if (activeCareplans.careplan.length > 0) {
         this.careplanId = activeCareplans.careplan[0].id;
         this.sessionId = (await this.sessionService.createNewSession(
           this.careplanId
-          )) as string;
-          this.router.navigate(["/app/session/", this.sessionId]);
-        }
+        )) as string;
+        this.router.navigate(["/app/session/", this.sessionId]);
+      }
     } else {
       this.sessionId = (await this.sessionService.createNewSession(
         this.careplanId
@@ -127,5 +141,49 @@ export class HomeComponent implements OnInit, AfterViewInit {
   async loginMusic() {
     const sound = new Audio("assets/sounds/Sound Health Soundscape_LogIn.mp3");
     sound.play();
+  }
+
+  nth(d: number) {
+    if (d > 3 && d < 21) return "th";
+    switch (d % 10) {
+      case 1:
+        return "st";
+      case 2:
+        return "nd";
+      case 3:
+        return "rd";
+      default:
+        return "th";
+    }
+  }
+
+  initMonthlyBar() {
+    d3.select(".progress").select("svg").remove();
+    let svg = d3
+      .select(".progress")
+      .append("svg")
+      .attr("height", "100%")
+      .attr("width", "100%");
+    svg
+      .append("rect")
+      .attr("class", "bg-rect")
+      .attr("rx", 10)
+      .attr("ry", 10)
+      .attr("fill", "#CBD5E0")
+      .attr("height", "100%")
+      .attr("width", "100%");
+    let progress = svg
+      .append("rect")
+      .attr("class", "progress-rect")
+      .attr("fill", "#00BD3E")
+      .attr("height", "100%")
+      .attr("width", 0)
+      .attr("rx", 10)
+      .attr("ry", 10)
+      .attr("y", 0);
+    progress
+      .transition()
+      .duration(500)
+      .attr("width", this.monthlyCompletionPercent + "%");
   }
 }
