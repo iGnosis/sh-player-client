d3e3ba899694ceafc3232583f1715fb1b8bf2382
vigneshrajj <vigneshhrajj@gmail.com>
2022-09-21 17:08:32 +0530

    test: add tests for start component

--- src/app/pages/auth/start/start.component.spec.ts
+++ src/app/pages/auth/start/start.component.spec.ts
@@ -1,10 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { StartComponent } from './start.component';
 
 describe('StartComponent', () => {
+  let router: Router;
   let component: StartComponent;
   let fixture: ComponentFixture<StartComponent>;
 
@@ -18,11 +20,36 @@ describe('StartComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(StartComponent);
+    router = TestBed.inject(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to signup page', () => {
+    const spy = spyOn(router, 'navigate');
+
+    component.signUp();
+
+    expect(spy).toHaveBeenCalledWith(['public/login']);
+  });
+
+  it('should navigate to login page', () => {
+    const spy = spyOn(router, 'navigate');
+
+    component.login();
+
+    expect(spy).toHaveBeenCalledWith(['public/login']);
+  });
+
+  it('should update carousel slide', () => {
+    component.carouselSlide = 1;
+
+    component.updateCarouselSlide(2);
+    
+    expect(component.carouselSlide).toEqual(2);
   });
 });
