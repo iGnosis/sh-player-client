198762064a6a51eb05f7e576e12b22a00c620939
vigneshrajj <vigneshhrajj@gmail.com>
2022-11-09 14:13:41 +0530

    fix: remove moving tones for stage and prod

--- src/app/services/careplan/careplan.service.ts
+++ src/app/services/careplan/careplan.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { GqlConstants } from "src/app/services/gql-constants/gql-constants.constants";
 import { GraphqlService } from "src/app/services/graphql/graphql.service";
+import { environment } from "src/environments/environment";
 
 @Injectable({
   providedIn: "root",
@@ -12,6 +13,13 @@ export class CareplanService {
     const games = await this.graphqlService.gqlRequest(
       GqlConstants.GET_AVAILABLE_GAMES
     );
+
+    const allowMovingTones = environment.name !== "prod" && environment.name !== "stage";
+
+    if (!allowMovingTones && games?.game_name) {
+      games.game_name = games.game_name.filter((game: { name: string }) => game.name !== "moving_tones");
+    }
+
     return games;
   }
 }
