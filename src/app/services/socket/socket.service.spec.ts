81bbb798e8ea2f9486ef78365fd2346f26fb4d26
vigneshrajj <vigneshhrajj@gmail.com>
2023-02-06 12:22:54 +0530

    test: 💍 fix failing test

--- src/app/services/socket/socket.service.spec.ts
+++ src/app/services/socket/socket.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { SocketService } from './socket.service';
@@ -6,7 +7,9 @@ describe('SocketService', () => {
   let service: SocketService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(SocketService);
   });
 
